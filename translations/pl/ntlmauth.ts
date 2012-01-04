433e51249d86be3b6b66279a129514882a2106b9
Sergey Potapov <potapov.s.a@gmail.com>
2012-01-04 14:22:35 +0400

    Поправлен скрипт сборки и установки локализаций, который перестал работать в Qt 4.8.

---
+++ translations/pl/ntlmauth.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" sourcelanguage="en">
+<context>
+    <name>NtlmAuth</name>
+    <message>
+        <source>Failed to process NTLM authorization</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wrong SASL authentication response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to start NTLM authorization</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Secure connection is not established</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NtlmAuthPlugin</name>
+    <message>
+        <source>NTLM Authentication</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allows to log in to Jabber server using NTLM authentication</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
